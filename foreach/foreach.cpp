3b197e1d78ea03895d825d336c866ecfb8f2e29b
Anna <annahasagun@yahoo.com>
2023-09-23 22:33:08 +0300

    Initial

---
+++ foreach/foreach.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+template <typename I, typename F>
+void parallel_for_each(I first, I last, const F& func)
+{
+    auto size = std::distance(first, last);
+    if (size <= 1)
+    {
+        func(*first);
+        return;
+    }
+    auto future = std::async(parallel_for_each<I, F>, first, std::next(first, size / 2), func);
+    parallel_for_each<I, F>(std::next(first, size / 2), last, func);
+}
+
+template <typename T>
+void display(T& container)
+{
+    for (auto el : container) {
+        std::cout << el << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> vec { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+    auto increase = [](int& num)
+    {
+        num++;
+    };
+    std::cout << "Before func: " << std::endl;
+    display(vec);
+    parallel_for_each(vec.begin(), vec.end(), increase);
+    std::cout << "After func: " << std::endl;
+    display(vec);
+}
+
---
+++ sort/sort.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+
+void display(const std::vector<int>& vec) 
+{
+    for (auto i : vec) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+void find_min(int start, std::vector<int>& vec, std::promise<int> prom) 
+{
+    int min = start;
+    for (int j = start; j < vec.size(); ++j) 
+    {
+        min = (vec.at(j) < vec.at(min)) ? j : min;
+    }
+    prom.set_value(min);
+
+};
+
+void sort(std::vector<int>& vec) {
+    int temp = 0;
+    int next_min = 0;
+    for (int i = 0; i < vec.size(); ++i) 
+    {
+        std::promise<int> prom;
+        std::future<int> ft = prom.get_future();
+        std::future<void> future = std::async(std::launch::async, find_min, i, std::ref(vec), std::move(prom));
+        future.wait();
+        next_min = ft.get();
+        temp = vec.at(next_min);
+        vec.at(next_min) = vec.at(i);
+        vec.at(i) = temp;
+    }
+}
+
+int main()
+{
+    std::vector<int> vec = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+    std::cout << "Before sort: " << std::endl;
+    display(vec);
+    sort(vec);
+    display(vec);
+}
+
